cf54b29c5b2142fae5331383b64c48d7ce1676b2
森田大二郎 <mority@MacBook-Pro.local>
2023-05-11 23:01:12 +0900

    modify variables and outputs

--- env/dev/outputs.tf
+++
@@ -1,4 +0,0 @@
-output "vpc_output_all" {
-  value       = module.vpc
-  description = "all output from vpc module"
-}
--- env/dev/variables.tf
+++ env/dev/variables.tf
@@ -1,27 +1,5 @@
-################################################################################
-# Common
-################################################################################
 variable "common_system" {
   type        = string
   description = "System Name"
 }
 
-################################################################################
-# Network
-################################################################################
-variable "vpc_cidr" {
-  type        = string
-  description = "VPC CIDR"
-}
-variable "vpc_azs" {
-  type        = list(string)
-  description = "Availabirity Zone"
-}
-variable "vpc_private_subnets" {
-  type        = list(string)
-  description = "Private Subnets CIDR"
-}
-variable "vpc_public_subnets" {
-  type        = list(string)
-  description = "Public Subnets CIDR"
-}
