c49a7e52c1d216d2eefa56da3359508e864ac3c4
森田大二郎 <mority@MacBook-Pro.local>
2023-05-15 11:33:17 +0900

    add EKS

--- env/dev/compute.tf
+++ env/dev/compute.tf
@@ -24,6 +24,6 @@ module "eks" {
 
   vpc_id                   = module.vpc.vpc_id
   subnet_ids               = module.vpc.private_subnets
-  control_plane_subnet_ids = module.vpc.pubric_subnets
+  control_plane_subnet_ids = module.vpc.private_subnets
 
 }
