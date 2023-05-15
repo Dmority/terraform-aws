798e4d88e61f6246b5e73efeb897055442d7d379
森田大二郎 <mority@MacBook-Pro.local>
2023-05-15 11:29:28 +0900

    add EKS

---
+++ env/dev/compute.tf
@@ -0,0 +1,29 @@
+################################################################################
+# EKS
+################################################################################
+module "eks" {
+  source  = "terraform-aws-modules/eks/aws"
+  version = "19.13.1"
+
+  cluster_name    = "${local.prefix}-eks-main"
+  cluster_version = "1.26"
+
+  cluster_endpoint_public_access = true
+
+  cluster_addons = {
+    coredns = {
+      most_recent = true
+    }
+    kube-proxy = {
+      most_recent = true
+    }
+    vpc-cni = {
+      most_recent = true
+    }
+  }
+
+  vpc_id                   = module.vpc.vpc_id
+  subnet_ids               = module.vpc.private_subnets
+  control_plane_subnet_ids = module.vpc.pubric_subnets
+
+}
